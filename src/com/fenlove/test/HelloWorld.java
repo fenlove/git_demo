4756cf9bc1aef8695290ca8681137a8ecded1281
fenlove <376116144@qq.com>
2015-11-19 11:57:40 +0800

    首次提交到本地仓库

---
+++ src/com/fenlove/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fenlove.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("_)(_ 欢迎来到git世界...");
+
+	}
+
+}
